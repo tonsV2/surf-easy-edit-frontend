e85f15826b78b578622f6cfe1cacdeda96ccd3bf
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-23 10:05:44 +0200

    Better markup.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <br />
-    <router-link to="/add">Add</router-link> -
-    <router-link to="/list">List</router-link> -
-    <router-link to="/login" v-if="!authenticated">Login</router-link>
-    <router-link to="/logout" v-if="authenticated">Logout</router-link>
+    <router-link to="/">
+      <img src="./assets/logo.png">
+    </router-link>
+    <div>
+      <router-link to="/add">Add</router-link> -
+      <router-link to="/list">List</router-link> -
+      <router-link to="/login" v-if="!authenticated">Login</router-link>
+      <router-link to="/logout" v-if="authenticated">Logout</router-link>
+    </div>
     <router-view></router-view>
   </div>
 </template>
