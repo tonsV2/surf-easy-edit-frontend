7b8c500210cdc0de9d9fa2122d3e8782e502d49f
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-04-25 17:34:55 +0200

    Updated readme and config api url to include schema.

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 
 export default {
   urls: {
-    api: location.hostname.endsWith('localhost') ? 'http://localhost:8080/api' : 'http://' + location.hostname + '/api'
+    api: location.hostname.endsWith('localhost') ? 'http://localhost:8080/api' : location.origin + '/api'
   },
   getUrl (ref) {
     return this.urls[ref]
