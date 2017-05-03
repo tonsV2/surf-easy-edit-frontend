47cfc613dcb16981d7bd3ba6bf3e634d45d720f8
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-05-03 12:25:17 +0200

    Added toast and different input element.

--- src/components/Edit.vue
+++ src/components/Edit.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="editor">
     <h1>Digital signage content</h1>
-    <textarea v-model="content" class="content"></textarea>
-    <button @click="submit()" class="submit">Submit</button>
+    <md-input-container>
+      <md-textarea v-model="content"></md-textarea>
+      <md-button @click.native="submit()" class="md-raised md-primary">Submit</md-button>
+    </md-input-container>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+import Toasted from 'vue-toasted'
+
 import editService from '../services/EditService'
+
+Vue.use(VueMaterial)
+Vue.use(Toasted)
 
 export default {
   methods: {
     submit () {
       editService.saveEdit(this.$route.query.id, this.content).then(content => {
         this.content = content
+        this.$toasted.show('Updated!', {
+          theme: 'outline',
+          position: 'top-right',
+          duration: 2000
+        })
       })
     }
   },
