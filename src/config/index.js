c9c68cbb1796b8c5ab21a9491907b619093ea81f
Sebastian Jensen <sebastianthegreatful@gmail.com>
2018-02-24 01:42:27 +0100

    Semi hardcode api url to hostname:8080/api

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@
 export default {
   urls: {
     base: location.origin,
-    api: location.hostname.endsWith('localhost') ? 'http://localhost:8080/api' : location.origin + '/api'
+    // api: location.hostname.endsWith('localhost') ? 'http://localhost:8080/api' : location.origin + ':' + location.port + '/api'
+    api: location.origin + ':8080/api'
   },
   getUrl (ref) {
     return this.urls[ref]
